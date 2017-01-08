d81e8e3621e5cf3236bb766d85340dddcf86ad08
Segfred <1104334344@qq.com>
2017-01-08 14:02:15 +0900

    Update binode.h

--- bitree/binode.h
+++ bitree/binode.h
@@ -6,20 +6,5 @@ class BiNode
 public:
 	T data;
 	BiNode<T> *lchild, *rchild;
-    //~BiNode();
 };
-
-/*template<typename T>
-BiNode<T>::BiNode()
-{
-    lchild=new BiNode<T>;
-    rchild=new BiNode<T>;
-}*/
-
-/*template<typename T>
-BiNode<T>::~BiNode()
-{
-    if(lchild!=nullptr) delete lchild;
-    if(rchild!=nullptr) delete rchild;
-}*/
 #endif
