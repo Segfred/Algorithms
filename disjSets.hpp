42aa88f2795ec6f826997b4f06ec338da7bd4d34
Segfred <1104334344@qq.com>
2017-04-05 19:02:04 +0900

    disjSets(union find)

--- disjSets.hpp
+++ disjSets.hpp
@@ -1,5 +1,5 @@
 //
-//  main.cpp
+//  disjSets.cpp
 //  disjSets
 //
 //  Created by Asmita on 2017/04/05.
@@ -15,32 +15,37 @@ template<typename DType>
 class disjSets{
 public:
     explicit disjSets(const vector<DType> &nodes);
-    DType findParent(DType& nodeName) const;
+    DType findParent(DType& nodeName) ;
     void unionSets(DType& node1, DType& node2);
     inline bool isConnected(DType& node1, DType& node2)
     {
         return findParent(node1)==findParent(node2);
     }
+    inline int getCount()
+    {
+        return count_;
+    }
 private:
     unordered_map<DType, DType> roots_;
     unordered_map<DType, bool> added_;
-    int count_;
+    int count_=0;
 };
 
 template<typename DType>
 disjSets<DType>::disjSets(const vector<DType> &nodes)
-{
+{   //count_=0;
     for(auto const &elem:nodes)
     {
-        roots_[elem]=-1;
-        if(!added_[elem]) ++count_;
+        roots_[elem]=elem;
+        //if(!added_[elem])
+            ++count_;
     }
 }
 
 template<typename DType>
-DType disjSets<DType>::findParent(DType& nodeName) const
+DType disjSets<DType>::findParent(DType& nodeName)
 {
-    if(roots_[nodeName]<0) return nodeName;
+    if(roots_[nodeName]==nodeName) return nodeName;
     else return roots_[nodeName]= findParent(roots_[nodeName]);
 }
 
@@ -49,7 +54,8 @@ void disjSets<DType>::unionSets(DType& node1, DType& node2) //union by size
 {
     DType root1=findParent(node1);
     DType root2=findParent(node2);
-    if(root1!=root2)
+    roots_[root2]=root1;
+    /*if(root1!=root2)
     {
         if(roots_[root1]<roots_[root2]) //root1 is deeper, so let root2 move to root1
         {
@@ -62,7 +68,7 @@ void disjSets<DType>::unionSets(DType& node1, DType& node2) //union by size
             roots_[root2]+=std::move(roots_[root1]);
             roots_[root1]=root2;
         }
-    }
+    }*/
 }
 
 #endif
