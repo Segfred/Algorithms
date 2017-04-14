4032bb46f49f58d5f573b9d40692db309bddfad5
Segfred <1104334344@qq.com>
2017-04-14 15:55:19 +0900

    Update BSNode_dele.cpp

--- BSNode_dele.cpp
+++ BSNode_dele.cpp
@@ -15,8 +15,8 @@ void removeNode1(BSTNode *& p)
     }  
     else//left and right children all exist      
     {  
-        BSTNode *s = NULL;  
-        q = p;  
+        BSTNode *s = NULL;  //since p is pointer's reference, we cannot move p, so a temporary pointer like s is a must
+        q = p;  //q stores the parent pointer
         s = p->lchild;       //root node of the left subtree  
         while(s->rchild) //find the precursor node of inorder traversal                           
         {                     
@@ -30,7 +30,7 @@ void removeNode1(BSTNode *& p)
         }  
         else               
         {  
-            q->lchild = s->lchild; 
+            q->lchild = s->lchild; // no node of p->lchild->rchild exists
         }  
         delete s;          
     }  
