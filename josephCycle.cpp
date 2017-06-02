f4d6177e8a185ab7e4e4664de46cff5e5d20d9d3
Segfred <1104334344@qq.com>
2017-06-02 12:38:26 +0900

    joseph cycle

---
+++ josephCycle.cpp
@@ -0,0 +1,15 @@
+//dp[i]=(dp[i-1]+k)%i,dp[i]表示i个人的时候最后剩下人的序号，或者说i个人做约瑟夫环游戏最后的人剩下的下标
+int getLast(int n, int k){
+if(n<1||k<1) return 0;
+int res=0;//int res=1是错的，考虑n=2,k=1，最后2%2会变成0，因此加1必须放在最后
+for(int i=2;i<=n;++i) res=(res+k)%i;//剩最后一个生会变成0, so the index must begin from 0
+return res+1;//return res(之前res=1错)
+}
+
+/*wrong solution
+int getLast(int n, int k){
+if(n<1||k<1) return 0;
+int res=1;//int res=1是错的，考虑n=2,k=1，最后2%2会变成0，因此加1必须放在最后
+for(int i=2;i<=n;++i) res=(res+k)%i;//剩最后一个生会变成0, so the index must begin from 0
+return res;//return res(之前res=1错)
+}
