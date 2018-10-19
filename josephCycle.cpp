39a267a148dae7ee9fcea59a426dfd02db371e42
Segfred <1104334344@qq.com>
2018-10-19 11:11:36 +0800

    Update josephCycle.cpp

--- josephCycle.cpp
+++ josephCycle.cpp
@@ -1,4 +1,5 @@
 //dp[i]=(dp[i-1]+k)%i,dp[i]表示i个人的时候最后剩下人的序号，或者说i个人做约瑟夫环游戏最后的人剩下的下标
+//http://qiemengdao.iteye.com/blog/1264485 f1 = 0，只有一个人的时候剩的编号为0
 int getLast(int n, int k){
 if(n<1||k<1) return 0;
 int res=0;//int res=1是错的，考虑n=2,k=1，最后2%2会变成0，因此加1必须放在最后
